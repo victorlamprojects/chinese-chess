1060de3c4aae0d219c13a1858b746bc4fe7b940c
Herman Wong <herman.wpt@gmail.com>
2022-12-22 01:30:04 +0800

    refactor: ChessUtil

--- common/ChessUtil.py
+++ common/ChessUtil.py
@@ -22,7 +22,6 @@ VALIDS = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
-
 N_MOVE = [[-2, -1], [-2, 1], [2, -1], [2, 1], [-1, -2], [1, -2], [-1, 2], [1, 2]]
 B_MOVE = [[-2, -2], [2, -2], [-2, 2], [2, 2]]
 A_MOVE = [[1, -1], [-1, 1], [-1, -1], [1, 1]]
@@ -30,20 +29,18 @@ K_MOVE = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 MOVE_MAP = {"k": K_MOVE, "a": A_MOVE, "b": B_MOVE, "n": N_MOVE}
 
 
-def get_opponent(current_player):
-    if current_player == "b":
-        return "r"
-    return "b"
+def get_opponent(current_player) -> str:
+    return "r" if current_player == "b" else "b"
 
-def get_king_position(p, board):
-    king = None
+
+def get_king_position(current_player, board) -> tuple:
     for i in range(10):
         for j in range(3, 6):
-            if board[i][j] == p + "k":
-                king = (i, j)
-    return king
+            if board[i][j] == current_player + "k":
+                return (i, j)
 
-def get_king_advisory_possible_moves(i, j, board):
+
+def calculate_king_advisory_possible_moves(i, j, board) -> list:
     moves = []
     current_player = board[i][j][0]
     for dr, dc in MOVE_MAP[board[i][j][1]]:
@@ -53,7 +50,8 @@ def get_king_advisory_possible_moves(i, j, board):
             moves.append((r, c))
     return moves
 
-def get_bishop_possible_moves(i, j, board):
+
+def calculate_bishop_possible_moves(i, j, board) -> list:
     moves = []
     current_player = board[i][j][0]
     for dr, dc in MOVE_MAP[board[i][j][1]]:
@@ -62,139 +60,70 @@ def get_bishop_possible_moves(i, j, board):
         if (
             VALIDS[r + 2][c + 2] >= 2
             and board[r][c][0] != current_player
-            and board[i + int(math.fmod((1 + dr), 2))][j + int(math.fmod((1 + dc), 2))] == "--"
-        ):
-            moves.append((r, c))
-    return moves
-
-def get_knight_possible_moves(i, j, board):
-    moves = []
-    current_player = board[i][j][0]
-    for m in MOVE_MAP[board[i][j][1]]:
-        r = m[0] + i
-        c = m[1] + j
-        if (
-            VALIDS[r + 2][c + 2] >= 1
-            and board[r][c][0] != current_player
-            and board[i + int(math.fmod(1 + m[0], 2))][j + int(math.fmod(1 + m[1], 2))]
+            and board[i + int(math.fmod((1 + dr), 2))][j + int(math.fmod((1 + dc), 2))]
             == "--"
         ):
             moves.append((r, c))
     return moves
 
-def get_rook_possible_moves(i, j, board):
-    r = i
-    c = j
-    moves = []
-    current_player = board[i][j][0]
-    # Check vertical direction
-    r = i - 1
-    while VALIDS[r + 2][c + 2] and board[r][c][0] != current_player:
-        moves.append((r, c))
-        if board[r][c] != "--":
-            break
-        r -= 1
-    r = i + 1
-    while VALIDS[r + 2][c + 2] and board[r][c][0] != current_player:
-        moves.append((r, c))
-        if board[r][c] != "--":
-            break
-        r += 1
-    # Check horizontal direction
-    r = i
-    c = j - 1
-    while VALIDS[r + 2][c + 2] and board[r][c][0] != current_player:
-        moves.append((r, c))
-        if board[r][c] != "--":
-            break
-        c -= 1
-    c = j + 1
-    while VALIDS[r + 2][c + 2] and board[r][c][0] != current_player:
-        moves.append((r, c))
-        if board[r][c] != "--":
-            break
-        c += 1
-    return moves
 
-
-def get_cannon_possible_moves(i, j, board):
-    def get_next_opponent(x, y, dir_i, dir_j, p, board):
-        x += dir_i
-        y += dir_j
-        while VALIDS[x + 2][y + 2] >= 1:
-            if board[x][y][0] == get_opponent(p):
-                return (x, y)
-            x += dir_i
-            y += dir_j
-        return (-1, -1)
+def calculate_cannon_possible_moves(i, j, board) -> list:
+    def get_next_chess(r, c, dr, dc, board):
+        r, c = r + dr, c + dc
+        while VALIDS[r + 2][c + 2]:
+            if board[r][c] != "--":
+                return (r, c)
+            r, c = r + dr, c + dc
+        return None
 
     moves = []
     current_player = board[i][j][0]
     opponent = get_opponent(current_player)
-    # Check vertical direction
-    r = i - 1
-    c = j
-    while VALIDS[r + 2][c + 2]:
-        # stop here
-        if board[r][c] != "--":
-            temp = get_next_opponent(r, c, -1, 0, current_player, board)
-            if (
-                temp[0] != -1
-                and temp[1] != -1
-                and board[temp[0]][temp[1]][0] == opponent
-            ):
-                moves.append(temp)
-            break
-        moves.append((r, c))
-        r -= 1
-    r = i + 1
-    while VALIDS[r + 2][c + 2]:
-        # stop here
-        if board[r][c] != "--":
-            temp = get_next_opponent(r, c, 1, 0, current_player, board)
-            if (
-                temp[0] != -1
-                and temp[1] != -1
-                and board[temp[0]][temp[1]][0] == opponent
-            ):
-                moves.append(temp)
-            break
-        moves.append((r, c))
-        r += 1
-    # Check horizontal direction
-    r = i
-    c = j - 1
-    while VALIDS[r + 2][c + 2]:
-        # stop here
-        if board[r][c] != "--":
-            temp = get_next_opponent(r, c, 0, -1, current_player, board)
-            if (
-                temp[0] != -1
-                and temp[1] != -1
-                and board[temp[0]][temp[1]][0] == opponent
-            ):
-                moves.append(temp)
-            break
-        moves.append((r, c))
-        c -= 1
-    c = j + 1
-    while VALIDS[r + 2][c + 2]:
-        # stop here
-        if board[r][c] != "--":
-            temp = get_next_opponent(r, c, 0, 1, current_player, board)
-            if (
-                temp[0] != -1
-                and temp[1] != -1
-                and board[temp[0]][temp[1]][0] == opponent
-            ):
-                moves.append(temp)
-            break
-        moves.append((r, c))
-        c += 1
+    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for dr, dc in dirs:
+        r, c = i + dr, j + dc
+        while VALIDS[r + 2][c + 2]:
+            if board[r][c] != "--":
+                next_chess = get_next_chess(r, c, dr, dc, board)
+                if next_chess and board[next_chess[0]][next_chess[1]][0] == opponent:
+                    moves.append(next_chess)
+                break
+            moves.append((r, c))
+            r, c = r + dr, c + dc
     return moves
 
 
-def get_pawn_possible_moves(i, j, board):
+def calculate_knight_possible_moves(i, j, board) -> list:
+    moves = []
+    current_player = board[i][j][0]
+    for dr, dc in MOVE_MAP[board[i][j][1]]:
+        r = dr + i
+        c = dc + j
+        if (
+            VALIDS[r + 2][c + 2]
+            and board[r][c][0] != current_player
+            and board[i + int(math.fmod(1 + dr, 2))][j + int(math.fmod(1 + dc, 2))]
+            == "--"
+        ):
+            moves.append((r, c))
+    return moves
+
+
+def calculate_rook_possible_moves(i, j, board) -> list:
+    moves = []
+    current_player = board[i][j][0]
+    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for dr, dc in dirs:
+        r, c = i + dr, j + dc
+        while VALIDS[r + 2][c + 2] and board[r][c][0] != current_player:
+            moves.append((r, c))
+            if board[r][c] != "--":
+                break
+            r, c = r + dr, c + dc
+    return moves
+
+
+def calculate_pawn_possible_moves(i, j, board) -> list:
     moves = []
     current_player = board[i][j][0]
     move_dir = 1
@@ -212,24 +141,23 @@ def get_pawn_possible_moves(i, j, board):
     return moves
 
 
-# Return a list of possible moves of a chess
-def calculate_moves(i, j, board):
+def calculate_possible_moves(i, j, board) -> list:
     if board[i][j] == "--":
         return []
     if board[i][j][1] == "k" or board[i][j][1] == "a":
-        return get_king_advisory_possible_moves(i, j, board)
+        return calculate_king_advisory_possible_moves(i, j, board)
     elif board[i][j][1] == "b":
-        return get_bishop_possible_moves(i, j, board)
+        return calculate_bishop_possible_moves(i, j, board)
     elif board[i][j][1] == "n":
-        return get_knight_possible_moves(i, j, board)
+        return calculate_knight_possible_moves(i, j, board)
     elif board[i][j][1] == "r":
-        return get_rook_possible_moves(i, j, board)
+        return calculate_rook_possible_moves(i, j, board)
     elif board[i][j][1] == "c":
-        return get_cannon_possible_moves(i, j, board)
-    return get_pawn_possible_moves(i, j, board)
+        return calculate_cannon_possible_moves(i, j, board)
+    return calculate_pawn_possible_moves(i, j, board)
 
-# Check if the current player is in check in the chessboard
-def is_checked(p, board):
+
+def is_being_checked(p, board) -> bool:
     king = get_king_position(p, board)
     opponent = get_opponent(p)
     for i in range(10):
@@ -247,36 +175,36 @@ def is_checked(p, board):
                         if l == u:
                             return True
                         continue
-                moves = calculate_moves(i, j, board)
+                moves = calculate_possible_moves(i, j, board)
                 for move in moves:
                     if king == move:
                         return True
     return False
 
-def get_valid_moves_from_pseudo_moves(i, j, moves, current_player, board):
+
+def get_valid_moves(row, col, current_player, board) -> list:
+    if board[row][col][0] != current_player:
+        return []
+    board_copy = deepcopy(board)
     valid_moves = []
-    for x, y in moves:
-        destChess = board[x][y]
-        board[x][y], board[i][j] = board[i][j], "--"
-        if not is_checked(current_player, board):
+    for x, y in calculate_possible_moves(row, col, board_copy):
+        destChess = board_copy[x][y]
+        board_copy[x][y], board_copy[row][col] = board_copy[row][col], "--"
+        if not is_being_checked(current_player, board_copy):
             valid_moves.append((x, y))
-        board[x][y], board[i][j] = destChess, board[x][y]
+        board_copy[x][y], board_copy[row][col] = destChess, board_copy[x][y]
     return valid_moves
 
-def is_checkmated(current_player, board):
+
+def is_checkmated(current_player, board) -> bool:
     for i in range(10):
         for j in range(9):
             if board[i][j][0] == current_player:
-                valid_moves = get_valid_moves_from_pseudo_moves(i, j, calculate_moves(i, j, board), current_player, board)
+                valid_moves = get_valid_moves(i, j, current_player, board)
                 if valid_moves:
                     return False
     return True
 
-def get_valid_moves(row, col, current_player, board):
-    if board[row][col][0] != current_player:
-        return []
-    board_copy = deepcopy(board)
-    return get_valid_moves_from_pseudo_moves(row, col, calculate_moves(row, col, board_copy), current_player, board_copy)
 
 def get_valid_moves_map(current_player, board):
     valid_moves_map = {}
@@ -286,16 +214,24 @@ def get_valid_moves_map(current_player, board):
                 valid_moves_map[(i, j)] = get_valid_moves(i, j, current_player, board)
     return valid_moves_map
 
+
 def calculate_score(board, current_player):
     return 0.5
+
 
 def calculate_score_for_x_y(i, j, x, y, current_player, board):
     board[x][y], board[i][j] = board[i][j], "--"
     return calculate_score(board, current_player)
 
+
 def get_best_move_and_score_for_i_j(i, j, valid_moves, current_player, board):
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_move = {executor.submit(calculate_score_for_x_y, i, j, x, y, current_player, deepcopy(board)): (x, y) for x, y in valid_moves}
+        future_to_move = {
+            executor.submit(
+                calculate_score_for_x_y, i, j, x, y, current_player, deepcopy(board)
+            ): (x, y)
+            for x, y in valid_moves
+        }
         best_move, best_score = (-1, -1), -1
         for future in concurrent.futures.as_completed(future_to_move):
             move = future_to_move[future]
@@ -304,9 +240,22 @@ def get_best_move_and_score_for_i_j(i, j, valid_moves, current_player, board):
                 best_move, best_score = move, score
         return best_move, best_score
 
+
 def get_best_move(current_player, board):
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_pos = {executor.submit(get_best_move_and_score_for_i_j, i, j, valid_moves, current_player, board): (i, j) for (i, j), valid_moves in get_valid_moves_map(current_player, board).items()}
+        future_to_pos = {
+            executor.submit(
+                get_best_move_and_score_for_i_j,
+                i,
+                j,
+                valid_moves,
+                current_player,
+                board,
+            ): (i, j)
+            for (i, j), valid_moves in get_valid_moves_map(
+                current_player, board
+            ).items()
+        }
         best_move, best_score = [(-1, -1), (-1, -1)], -1
         for future in concurrent.futures.as_completed(future_to_pos):
             pos = future_to_pos[future]
@@ -314,6 +263,7 @@ def get_best_move(current_player, board):
             if score > best_score:
                 best_move, best_score = [pos, move], score
     return best_move
+
 
 def encode_gameboard(current_player, board):
     def encode_chess_type(chess):
--- common/TestChessUtil.py
+++ common/TestChessUtil.py
@@ -1,16 +1,15 @@
 from ChessUtil import (
-    get_king_advisory_possible_moves,
-    get_bishop_possible_moves,
-    get_knight_possible_moves,
-    get_rook_possible_moves,
-    get_cannon_possible_moves,
-    get_pawn_possible_moves,
-    calculate_moves,
-    is_checked,
-    get_valid_moves_from_pseudo_moves,
+    calculate_king_advisory_possible_moves,
+    calculate_bishop_possible_moves,
+    calculate_cannon_possible_moves,
+    calculate_knight_possible_moves,
+    calculate_rook_possible_moves,
+    calculate_pawn_possible_moves,
+    calculate_possible_moves,
+    is_being_checked,
     is_checkmated,
     get_valid_moves,
-    get_best_move
+    get_best_move,
 )
 import unittest
 
@@ -78,51 +77,51 @@ class TestChessUtil(unittest.TestCase):
             ["--", "--", "--", "--", "rk", "--", "--", "--", "--"],
         ]
 
-    def test_get_king_possible_moves(self):
+    def test_calculate_king_possible_moves(self):
         i, j = 0, 4
-        moves = get_king_advisory_possible_moves(i, j, self.board)
+        moves = calculate_king_advisory_possible_moves(i, j, self.board)
         expected_moves = [(1, 4)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_advisory_possible_moves(self):
+    def test_calculate_advisory_possible_moves(self):
         i, j = 0, 3
-        moves = get_king_advisory_possible_moves(i, j, self.board)
+        moves = calculate_king_advisory_possible_moves(i, j, self.board)
         expected_moves = [(1, 4)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_bishop_possible_moves(self):
+    def test_calculate_bishop_possible_moves(self):
         i, j = 0, 2
-        moves = get_bishop_possible_moves(i, j, self.board)
+        moves = calculate_bishop_possible_moves(i, j, self.board)
         expected_moves = [(2, 0), (2, 4)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_bishop_river_possible_moves(self):
+    def test_calculate_bishop_river_possible_moves(self):
         i, j = 5, 2
-        moves = get_bishop_possible_moves(i, j, self.board2)
+        moves = calculate_bishop_possible_moves(i, j, self.board2)
         expected_moves = [(7, 0)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_knight_possible_moves(self):
+    def test_calculate_knight_possible_moves(self):
         i, j = 9, 1
-        moves = get_knight_possible_moves(i, j, self.board)
+        moves = calculate_knight_possible_moves(i, j, self.board)
         expected_moves = [(7, 0), (7, 2)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_knight_blocked_possible_moves(self):
+    def test_calculate_knight_blocked_possible_moves(self):
         i, j = 7, 6
-        moves = get_knight_possible_moves(i, j, self.board2)
+        moves = calculate_knight_possible_moves(i, j, self.board2)
         expected_moves = [(9, 7), (8, 8), (8, 4), (6, 4)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_rook_possible_moves(self):
+    def test_calculate_rook_possible_moves(self):
         i, j = 9, 0
-        moves = get_rook_possible_moves(i, j, self.board)
+        moves = calculate_rook_possible_moves(i, j, self.board)
         expected_moves = [(8, 0), (7, 0)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_cannon_possible_moves(self):
+    def test_calculate_cannon_possible_moves(self):
         i, j = 7, 1
-        moves = get_cannon_possible_moves(i, j, self.board)
+        moves = calculate_cannon_possible_moves(i, j, self.board)
         expected_moves = [
             (7, 0),
             (7, 2),
@@ -139,55 +138,50 @@ class TestChessUtil(unittest.TestCase):
         ]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_pawn_possible_moves(self):
+    def test_calculate_pawn_possible_moves(self):
         i, j = 6, 4
-        moves = get_pawn_possible_moves(i, j, self.board)
+        moves = calculate_pawn_possible_moves(i, j, self.board)
         expected_moves = [(5, 4)]
         self.assertCountEqual(expected_moves, moves)
 
     def test_calculate_moves(self):
         i, j = 9, 0
-        moves = calculate_moves(i, j, self.board)
+        moves = calculate_possible_moves(i, j, self.board)
         expected_moves = [(8, 0), (7, 0)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_is_checked_true(self):
-        self.assertTrue(is_checked(p="b", board=self.board2))
+    def test_is_being_checked_true(self):
+        self.assertTrue(is_being_checked(p="b", board=self.board2))
 
-    def test_is_checked_false(self):
-        self.assertFalse(is_checked(p="b", board=self.board3))
-    
-    def test_is_checked_special_case(self):
-        self.assertTrue(is_checked(p="b", board=self.board4))
+    def test_is_being_checked_false(self):
+        self.assertFalse(is_being_checked(p="b", board=self.board3))
 
-    def test_get_valid_moves_from_pseudo_moves_cant_move_case(self):
+    def test_is_being_checked_special_case(self):
+        self.assertTrue(is_being_checked(p="b", board=self.board4))
+
+    def test_get_valid_moves_cant_move_case(self):
         i, j = 0, 0
-        moves = get_valid_moves_from_pseudo_moves(i, j, calculate_moves(i, j, self.board2), "b", self.board2)
+        moves = get_valid_moves(i, j, "b", self.board2)
         expected_moves = []
         self.assertCountEqual(expected_moves, moves)
-    
-    def test_get_valid_moves_from_pseudo_moves_defend_king_case(self):
-        i, j = 0, 2
-        moves = get_valid_moves_from_pseudo_moves(i, j, calculate_moves(i, j, self.board2), "b", self.board2)
-        expected_moves = [(2, 4)]
-        self.assertCountEqual(expected_moves, moves)
 
-    def test_is_checkmated_true(self):
-        self.assertTrue(is_checkmated("b", self.board5))
-    
-    def test_is_checkmated_false(self):
-        self.assertFalse(is_checkmated("b", self.board2))
-
-    def test_get_valid_moves(self):
+    def test_get_valid_moves_defend_king_case(self):
         i, j = 0, 2
         moves = get_valid_moves(i, j, "b", self.board2)
         expected_moves = [(2, 4)]
         self.assertCountEqual(expected_moves, moves)
 
-    def test_get_best_move(self):
-        expected_best_move = []
-        best_move = get_best_move("b", self.board2)
-        self.assertCountEqual(expected_best_move, best_move)
+    def test_is_checkmated_true(self):
+        self.assertTrue(is_checkmated("b", self.board5))
+
+    def test_is_checkmated_false(self):
+        self.assertFalse(is_checkmated("b", self.board2))
+
+    # def test_get_best_move(self):
+    #     expected_best_move = []
+    #     best_move = get_best_move("b", self.board2)
+    #     self.assertCountEqual(expected_best_move, best_move)
+
 
 if __name__ == "__main__":
     unittest.main()
